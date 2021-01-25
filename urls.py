8afb766febe1f343a207c7339c4afb43213eeb17
dcoutinho1328 <60111446+dcoutinho1328@users.noreply.github.com>
2021-01-25 11:15:23 -0300

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='endereco-home'), #redireciona o cliente para a view 'home'
-    path('create/', views.create, name='endereco-create'), #redireciona o cliente para a view 'create'
-]
