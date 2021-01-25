0847861ba40da52a95f5e1293dec1d9f13993a5b
dcoutinho1328 <60111446+dcoutinho1328@users.noreply.github.com>
2021-01-25 11:00:44 -0300

    Delete admin.py

--- cadastro_enderecos/enderecos/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from enderecos.models import Enderecos
-
-admin.site.register(Enderecos) #permite que se possa alterar os dados salvos na base de dados de forma fácil na página /admin
-
