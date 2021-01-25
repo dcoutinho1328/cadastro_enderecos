c0036f21e68a645a420afcfe9af3fe0e46c23f60
dcoutinho1328 <60111446+dcoutinho1328@users.noreply.github.com>
2021-01-25 10:59:02 -0300

    Add files via upload

---
+++ cadastro_enderecos/enderecos/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from enderecos.models import Enderecos
+
+admin.site.register(Enderecos) #permite que se possa alterar os dados salvos na base de dados de forma fácil na página /admin
+
